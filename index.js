601e7dbf5ba0fc5cf6bc5ce44166049349099d1b
shivam chourasia <91719925+shivamchourasia233022@users.noreply.github.com>
2022-10-10 13:26:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function connect(){
+//   account= await window.ethereum.request({method:"eth_requestAccounts"}).catch((err)=>{
+//         console.log(err.code)
+//     })
+    console.log("hello");
+}
